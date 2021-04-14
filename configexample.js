a743b3335386471c80d1078623665543ecb9ab3d
sophiacheong <sophiaacheong@gmail.com>
2021-04-14 14:27:20 -0700

    editied configexample file

--- configexample.js
+++ configexample.js
@@ -1,6 +1,6 @@
 // --- DO NOT COPY & PASTE INTO THIS CONFIG FILE. MAKE NEW ONE AND SAVE YOUR API KEY IN THERE ---//
 const Authorization = {
-  Authorization: 'API TOKEN',
+  Authorization: 'Bearer API TOKEN',
 };
 
 module.exports = Authorization;
